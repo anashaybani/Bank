7cc3c5dcda90fcf376a6fa93449171cfbaed1749
anashaybani <anashybani@gmail.com>
2025-07-21 20:28:47 +0330

    dateAutomation

--- transferwindow.cpp
+++ transferwindow.cpp
@@ -10,6 +10,7 @@ TransferWindow::TransferWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    connect(ui->transferButton, &QPushButton::clicked, this, &TransferWindow::on_transferButton_clicked);
     connect(ui->closeButton, &QPushButton::clicked, this, &TransferWindow::on_closeButton_clicked);
 }
 
@@ -19,6 +20,7 @@ TransferWindow::~TransferWindow()
 
 }
 
+#include <ctime>
 
 void TransferWindow::on_transferButton_clicked()
 {
@@ -46,36 +48,64 @@ void TransferWindow::on_transferButton_clicked()
         return;
     }
 
-    if (date.toStdString() > origin->getExpirationDate()) {
-        ui->statusLabel->setText("orgin card expired !");
+
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    int currentYear = now->tm_year + 1900;
+    int currentMonth = now->tm_mon + 1;
+
+
+    string expDateStr = origin->getExpirationDate();
+    int expYear = stoi(expDateStr.substr(0, 4));
+    int expMonth = stoi(expDateStr.substr(5, 2));
+
+
+    if (currentYear > expYear || (currentYear == expYear && currentMonth > expMonth)) {
+        ui->statusLabel->setText("Origin card expired!");
         return;
     }
+
+    string today = to_string(currentYear) + "-" + (currentMonth < 10 ? "0" + to_string(currentMonth) : to_string(currentMonth));
+
+    if (date.toStdString() != origin->getExpirationDate()) {
+        ui->statusLabel->setText(" wrong Origin card expire ddate!");
+        return;
+    }
+
+    double totalToday = CardToCardS.getTotalAmountForCardAndDate(originCard.toStdString(), today);
 
     double total = amount * 1.01;
     CardToCard temp;
     temp.setoriginCardNum(originCard.toStdString()) ;
-    temp.setdate(date.toStdString());
+    temp.setdate(today);
 
-    CardToCard* recent = CardToCardS.search(temp.display());
-    if (amount > 3000000 || total > origin->getBalance() || (recent && recent->getAmount() + amount > 6000000)) {
+    if (amount > 3000000 || total > origin->getBalance() || totalToday + amount > 6000000) {
         ui->statusLabel->setText("Amount over limit or balance!");
         return;
     }
 
     if (amount <= 100000) {
         if (password.toStdString() != origin->getStaticSecondPassword()) {
-            ui->statusLabel->setText("Incorrect password!");
+            ui->statusLabel->setText("Incorrect second password!");
             return;
         }
     } else {
-        std::string dynPass = origin->generateRandomDigits(6);
-        origin->setDynamicSecondPassword(dynPass);
-        QMessageBox::information(this, "Dynamic", QString::fromStdString("Dynamic password: " + dynPass));
-        if (password.toStdString() != dynPass) {
-            ui->statusLabel->setText("Incorrect password!");
+        if (origin->getDynamicSecondPassword().empty()) {
+            const string dynPass = origin->generateRandomDigits(5);
+            origin->setDynamicSecondPassword(dynPass);
+            QMessageBox::information(this, "Dynamic Password", QString::fromStdString("Dynamic password: " + dynPass));
             return;
         }
+
+        if (password.toStdString() != origin->getDynamicSecondPassword()) {
+            ui->statusLabel->setText("Incorrect dynamic password!");
+            origin->setDynamicSecondPassword("");
+            return;
+        }
+
+        origin->setDynamicSecondPassword("");
     }
+
 
     origin->setBalance(origin->getBalance() - total);
     dest->setBalance(dest->getBalance() + amount);
@@ -83,14 +113,14 @@ void TransferWindow::on_transferButton_clicked()
 
     CardToCard* transaction = new CardToCard();
     transaction->setoriginCardNum(originCard.toStdString());
-    transaction->setdate(date.toStdString());
-
-
+    transaction->setdestinationCardNum(destCard.toStdString());
+    transaction->setdate(today);
     transaction->setamount(amount);
     CardToCardS.add(transaction);
 
 
-    ui->statusLabel->setText("Successful transaction.");
+    QMessageBox::information(this, "SUCCESS","Successful transaction. ");
+    this->close();
 }
 
 
